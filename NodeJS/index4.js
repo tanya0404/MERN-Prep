e815d539102c76f802676f18272549be1d3ded98
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2026-02-19 16:45:22 +0530

    update

--- NodeJS/index4.js
+++ NodeJS/index4.js
@@ -1,13 +1,40 @@
 import express from 'express'
 const app=express()
 
+// app.get("/",(req,res)=>{
+//     const user={
+//         name:"tanya",
+//         email:"tp@gmail.com",
+//         role:"student"
+//     }
+//     res.json(user)
+// })
+
+app.use(express.json())
+
+let user=[{
+    id:1,name:"john",email:"john@gmail.com",role:"student"},
+{id:2,name:"Cbjed",email:"dwsd@gmail.com",role:"student"},{id:3,name:"wdd",email:"wsd@gmail.com",role:"admin"}]
+
 app.get("/",(req,res)=>{
-    const user={
-        name:"tanya",
-        email:"tp@gmail.com",
-        role:"student"
-    }
     res.json(user)
 })
+
+app.post("/",(req,res)=>{                    //here we are adding new object in array
+    const newuser=req.body
+    user.push(newuser)
+    res.json(user)
+})
+
+app.get("/:id",(req,res)=>{                   //find the id and printing that object only
+    const num=Number(req.params.id)
+    const find=user.find((e)=>
+        e.id===num
+    )
+
+
+    res.json(find)
+})
+
 
 app.listen(8080)
