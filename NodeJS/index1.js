f59f2856270ab047bfb7a27afba7f98bf903df3c
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2026-02-19 11:57:09 +0530

    update

--- NodeJS/index1.js
+++ NodeJS/index1.js
@@ -1,2 +1,25 @@
 
-console.log("hello world")
+import express from 'express';
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log(req.url);
+    res.send("hello");
+});
+
+app.get("/home", (req, res) => {
+    console.log(req.url.method)
+    res.send("home page");
+});
+
+app.get("/home/page1", (req, res) => {
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.body);
+    console.log(req.headers.authorization)
+    res.send("home page/page 1");
+});
+
+app.listen(8082, () => {
+    console.log("server started on port 8082");
+});
